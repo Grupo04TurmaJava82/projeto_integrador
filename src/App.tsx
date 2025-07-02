78cf6051ae9c1d045c8df709baf29d36909796e1
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-07-02 15:00:02 -0300

    Preparação do componente Home

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     <>
-      <Home
-        titulo="Componente Home"
-        texto="Este é o componente Home, que recebe props."
-      />
+      <Home />
     </>
   );
 }
