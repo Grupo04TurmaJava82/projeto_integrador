03266ad56787bcb13bc9bf5ec2afd365375a4e5f
Iago Zandone <iagozandone@gmail.com>
2025-07-03 14:27:28 -0300

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import "./App.css";
 import Sobre from "./components/sobre/Sobre";
+import ListaExercicios from "./pages/exercicios/ListaExercicios";
+import Metricas from "./components/metricas/Metricas";
+import FormExercicio from "./components/formexercicio/FormExercicio";
+import Metodologia from "./components/metodologia/Metodologia";
 
 function App() {
   return (
     <>
-      <Home />
-      <Sobre />
+    <BrowserRouter>
+      <Routes>
+      <Route	path="/" element={<Home />} />
+      <Route	path="/home" element={<Home />}	/>
+      <Route	path="/listaexercicios" element={<ListaExercicios />}	/>
+      <Route	path="/formexercicio" element={<FormExercicio />}	/>
+      <Route	path="/metodologia" element={<Metodologia />}	/>
+      <Route	path="/sobre" element={<><Metodologia /><Sobre /></>}	/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
